9042494fc2c07a18f719b87f2be04709b0392db5
lucca <luccasfs98@gmail.com>
2019-12-04 22:20:05 -0300

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCzKntEuAubKojuu80OJhX-WKwQb29pTn4",
+    authDomain: "passe-livre-documentos.firebaseapp.com",
+    databaseURL: "https://passe-livre-documentos.firebaseio.com",
+    projectId: "passe-livre-documentos",
+    storageBucket: "passe-livre-documentos.appspot.com",
+    messagingSenderId: "264820843241",
+    appId: "1:264820843241:web:8455808629dab0f94ec678",
+    measurementId: "G-G95MYQJT7C"
+  }
 };
